dee48826acb1f46f4dcb22155c32c59274d4339e
yeomboyeon <yeomboyeon1221@gmail.com>
2022-05-28 16:56:41 +0900

    게시판 중복 제거 구현(보완중)

--- src/App.java
+++ src/App.java
@@ -65,7 +65,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 
 				int id = Integer.parseInt(commandBits[2]);
-
+// article 하나를 가져와서 foundArticle 변수에 저장하고 id를 불러오겠다
 				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
@@ -134,32 +134,38 @@ public class App {
 			if (article.id == id) {
 				return i;
 			}
+// 검색해서 나온다면 인덱스 번호를 증가 
 			i++;
 		}
-		return -1; // 없다는 뜻의 인덱스를 불러올때 음수로 리턴
+// 없다는 뜻의 인덱스를 불러올때 음수로 리턴
+		return -1; 
 	}
 
-// 찾는 메서드 구현 진행중(3단계 구현)
+// 찾는 메서드 구현 진행중(3단계 구현) 
 	private Article getArticleById(int id) {
+// getArticleById 가 getArticleIndexById 에게 id 넘겨줄테니 인덱스 번호 알려줘
 		int index = getArticleIndexById(id);
 // 인덱스로 불러오되, 인덱스는 0부터 시작하기에 음수-1과 같지 않다면으로 함수 작성
+// 반복문을 사용하지 않고 바로 검색 가능.
 		if (index != -1) {
 			return articles.get(index);
 		}
+// 위에 반복문을 뒤져봤더니 없어서 없다라고 null로 리턴
 		return null;
 	}
 
-// 찾기 메서드 1단계
+// 찾기 메서드 1단계 찾기 기능이 중복되어 있기에 별도로 메서드를 구현하는 단계임
+// article 리스트에 글 수만큼 0부터 계속 반복해서 검색한다.글수만큼(글 번호가 9라면 8까지)
 //		for (int i = 0; i < articles.size(); i++) {  
 //			Article article = articles.get(i);
-//
+// 
 //			if (article.id == id) {
 //				return article;
 //			}
 //		}
-// 찾기 메서드 2단계
+// 찾기 메서드 2단계 (배열에서 써먹을 수 있는 코드작성법)
 //		for (Article article : articles) {
-//
+// fir each문과 유사
 //			if (article.id == id) {
 //				return article;
 //			}
